8089478116b5f27e9b3ea97396c7551d127626fd
alexfortin <fortin.alex14@gmail.com>
2022-10-19 03:45:06 -0500

    added all the questions and made it so you can loop through interns and engineers until your done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer")
 
-const teamQuestions = () => {
+const engineers = []
+const interns = []
+
+const managerQuestions = () => {
     return inquirer
         .prompt([
             {
@@ -43,8 +46,139 @@ const teamQuestions = () => {
         ])
 }
 
-teamQuestions()
+const choices = () => {
+    return inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "choice",
+                message: "What type of team member would you like to add?",
+                choices: ["Engineer", "Intern", "Assemble Team"]
+            }
+        ])
+        .then(choicedata => {
+            if(choicedata.choice === "Engineer"){
+                engineerQuestions()
+            }
+            else if(choicedata.choice === "Intern")
+            {
+                internQuestions()
+            }
+            else{
+                console.log(engineers)
+                console.log(interns)
+            }
+        })
+}
+
+const engineerQuestions = () => {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the engineer's name?",
+                validate: isManagerName => {
+                    if (isManagerName) { return true }
+                    else { return false }
+                }
+
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is the engineer's id?",
+                validate: isManagerId => {
+                    if (isManagerId) { return true }
+                    else { return false }
+                }
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the engineer's email?",
+                validate: isManagerEmail => {
+                    if (isManagerEmail) { return true }
+                    else { return false }
+                }
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "What is the engineer's GitHub username?",
+                validate: isOfficeNum => {
+                    if (isOfficeNum) { return true }
+                    else { return false }
+                }
+            }
+        ])
+        .then(engineerData => {
+            engineers.push(engineerData)
+            choices()
+        })
+}
+
+const internQuestions = () => {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is the intern's name?",
+                validate: isManagerName => {
+                    if (isManagerName) { return true }
+                    else { return false }
+                }
+
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What is the intern's id?",
+                validate: isManagerId => {
+                    if (isManagerId) { return true }
+                    else { return false }
+                }
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is the intern's email?",
+                validate: isManagerEmail => {
+                    if (isManagerEmail) { return true }
+                    else { return false }
+                }
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is the intern's school?",
+                validate: isOfficeNum => {
+                    if (isOfficeNum) { return true }
+                    else { return false }
+                }
+            }
+        ])
+        .then(internData => {
+            interns.push(internData)
+            choices()
+        })
+}
+
+managerQuestions()
     .then(data => {
         console.log(data)
     })
+    .then(choices)
+    // .then(choicedata => {
+    //     console.log(choicedata)
+    // })
+    // .then(engineerQuestions)
+    // .then(enginedata => {
+    //     console.log(enginedata)
+    // })
+    // .then(internQuestions)
+    // .then(interndata => {
+    //     console.log(interndata)
+    // })
     
